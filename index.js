d660f3a547f5a870abcc30de5aa0ed58d00324d6
Timothy Ruhle <timothy.ruhle@intellipharm.com.au>
2015-12-16 08:53:54 +1000

    Fixing up ES6 support

--- index.js
+++ index.js
@@ -23,12 +23,18 @@ exports['default'] = function (angularModule, futureRoutes) {
 
 		$futureStateProvider.stateFactory('load', ['$q', '$ocLazyLoad', 'futureState', function ($q, $ocLazyLoad, futureState) {
 			return $q.when(System['import'](futureState.src).then(function (loaded) {
+				// this is the angular module
 				if (loaded.name) {
 					return $ocLazyLoad.load(loaded);
-				} else {
-					var keys = Object.keys(loaded);
-					return $ocLazyLoad.load(loaded[keys[0] === '__esModule' ? keys[1] : keys[0]]);
 				}
+
+				// this is the ES6 default export
+				if (loaded.default) {
+					return $ocLazyLoad.load(loaded.default);
+				}
+
+				var keys = Object.keys(loaded);
+				return $ocLazyLoad.load(loaded[keys[0] === '__esModule' ? keys[1] : keys[0]]);
 			})).then(angular.noop);
 		}]);
 
